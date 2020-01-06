8555a44b547ea2daec588c7174269e7969a6f844
kyllo34 <kyllo34@gmail.com>
2020-01-06 11:50:48 -0800

    completed step 12

--- class-00/lab-b/lab/app.js
+++ class-00/lab-b/lab/app.js
@@ -124,14 +124,14 @@ let objectLit = () => ({
 console.log(objectLit());
 
 
-let sumAndProduct = (a, b)=>  {
+let sumAndProduct = (a, b) => {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
+console.log(sumAndProduct(3, 9));
 
 
 let message = function(name) {
